536f97758bd9b449ce0024b3e562b7592f22a903
Arnab Nath <arnabnath@sleekinfosolution.com>
2022-06-20 18:08:05 +0530

    changes

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -5,8 +5,4 @@ import '../src/LoanBasicDetails/BasicDetails.js';
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
-
-  it('Test Case',function(){
-    expect(1).to.be.equal(1);
-  });
 });
